9952d13115b992ca59700c19b0a299d974099301
Maxime Suret <msuret@dhatim.com>
2016-10-19 10:01:11 +0200

    add config logger

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ var loadLoggingConfig = require('sequelize').Promise.promisify(require('winston-
 module.exports = loadLoggingConfig(config.get('logging'))
     .then(function(){
         if(cluster.isMaster){
-            winston.loggers.get('default').info('using configration:', JSON.stringify(config, null, 2));
+            winston.loggers.get('config').info('using configration:', JSON.stringify(config, null, 2));
             //run pending migrations and fork clusters
             var secret = randomstring.generate(50);
             return require('./models').migrate()
