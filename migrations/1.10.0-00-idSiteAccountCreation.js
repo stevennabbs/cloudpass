7d8c82aff1670547ea16d9344b5fb783b6cba48c
Olivier Chédru <ochedru@dhatim.com>
2019-10-14 16:14:59 +0200

    Fix typo

--- migrations/1.10.0-00-idSiteAccountCreation.js
+++ migrations/1.10.0-00-idSiteAccountCreation.js
@@ -2,7 +2,7 @@
 module.exports = {
     up: function(migration, DataTypes, models) {
         return migration.addColumn(
-            'idSites',
+            'idSite',
             'accountCreation',
             {
                 type: DataTypes.BOOLEAN,
